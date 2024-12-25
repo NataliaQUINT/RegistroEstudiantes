06bcbab266411adafd203df41cdafb3328f83fd2
NataliaQuint <natalia.quinteros@inacapmail.cl>
2024-12-25 17:05:56 -0300

    commit corregido
    commit corregido

---
+++ RegistroEstudiantes.Modelos/Modelos/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroEstudiantes.Modelos.Modelos
+{
+    public class Estudiante
+    {
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Email { get; set; }
+        public int Edad { get; set; }
+        public Curso curso { get; set; }
+        public string NombreCompleto => $"{PrimerNombre}  {SegundoNombre} {ApellidoPaterno} {ApellidoMaterno}";
+    }
+}
